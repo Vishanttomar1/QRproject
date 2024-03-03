643a9beaed0e685a5f40d68ffa5418cc3abe0995
Vishant Tomar <tomarvishant781@gmail.com>
2024-03-03 19:43:57 +0530

    add some files

---
+++ app.js
@@ -0,0 +1,17 @@
+let imgbox = document.getElementById("imgbox");
+let QRimage = document.getElementById("QRimage");
+let QRtext = document.getElementById("QRtext");
+
+function generatQR(){
+if(QRtext.value.length >0){
+QRimage.src=" https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + QRtext.value;
+imgbox.classList.add("show-img");
+} else{
+QRtext.classList.add('error');
+setTimeout(()=>{
+    QRtext.classList.remove('error');
+},1000);
+
+}
+
+}
